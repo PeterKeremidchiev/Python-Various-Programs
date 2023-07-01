980f55dac21e996f0ec0c06ab10dbc45a7cdb557
Peter Keremidchiev <spartak97@abv.bg>
2023-07-01 22:46:24 +0300

    Code added from inheritance exercise

---
+++ python_OOP/ineheritance_exercise/need_for_speed/project/motorcycle.py
@@ -0,0 +1,4 @@
+from inheritance_exercise.need_for_speed.project.vehicle import Vehicle
+
+class Motorcycle(Vehicle):
+    pass
---
+++ python_OOP/ineheritance_exercise/need_for_speed/project/race_motorcycle.py
@@ -0,0 +1,4 @@
+from inheritance_exercise.need_for_speed.project.motorcycle import Motorcycle
+
+class RaceMotorcycle(Motorcycle):
+    DEFAULT_FUEL_CONSUMPTION = 8
