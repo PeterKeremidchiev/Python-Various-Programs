ebee73c656733692a5ffe7260e68a686e0e9d4f6
Peter Keremidchiev <spartak97@abv.bg>
2023-07-24 22:35:00 +0300

    Code from decorators exercise

---
+++ python_OOP/decorators_exercise/logged.py
@@ -0,0 +1,10 @@
+def logged(function):
+    def wrapper(*args):
+        return f"you called {function.__name__}{args}\n" \
+                f"it returned {function(*args)}"
+    return wrapper
+
+@logged
+def sum_func(a, b):
+    return a + b
+print(sum_func(1, 4))
