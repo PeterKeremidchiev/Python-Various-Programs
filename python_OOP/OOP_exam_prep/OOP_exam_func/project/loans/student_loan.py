718efd3d095d5f0c24e89ef3dfc9285d878698bc
Peter Keremidchiev <spartak97@abv.bg>
2023-08-08 21:47:46 +0300

    Code from OOP exam

---
+++ python_OOP/OOP_exam_prep/OOP_exam_func/project/loans/student_loan.py
@@ -0,0 +1,13 @@
+from project.loans.base_loan import BaseLoan
+
+
+class StudentLoan(BaseLoan):
+    def __init__(self):
+        super().__init__(interest_rate=1.5, amount=2000.0)
+
+    def increase_interest_rate(self):
+        self.interest_rate += 0.2
+
+# sl = StudentLoan()
+# sl.increase_interest_rate()
+# print(sl.interest_rate)
